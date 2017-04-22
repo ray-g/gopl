697a0cf92ea58b7a38ef7969113bffd3bf08a522
Ray Guo <ray_g@live.com>
2017-04-23 00:42:10 +0800

    ex7.16

---
+++ ch07/ex7.16/README.md
@@ -0,0 +1,3 @@
+# Exercise 7.16 (P205)
+
+Write a web-based calculator program.
---
+++ ch07/ex7.16/main.go
@@ -0,0 +1,53 @@
+// Evaluate an expression for http clients, accepting `expr` and `env` as query
+// parameters, eg: curl 'localhost:8000/?expr=x*y&env=x=3%20y=4'
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+
+	eval "github.com/ray-g/gopl/ch07/ex7.14"
+)
+
+func parseEnv(s string) (eval.Env, error) {
+	env := eval.Env{}
+	assignments := strings.Fields(s)
+	for _, a := range assignments {
+		fields := strings.Split(a, "=")
+		if len(fields) != 2 {
+			return env, fmt.Errorf("bad assignment: %s\n", a)
+		}
+		ident, valStr := fields[0], fields[1]
+		val, err := strconv.ParseFloat(valStr, 64)
+		if err != nil {
+			return env, fmt.Errorf("bad value for %s: %s\n", ident, err)
+		}
+		env[eval.Var(ident)] = val
+	}
+	return env, nil
+}
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		exprStr := r.FormValue("expr")
+		if exprStr == "" {
+			http.Error(w, "no expression", http.StatusBadRequest)
+			return
+		}
+		env, err := parseEnv(r.FormValue("env"))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		expr, err := eval.Parse(exprStr)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		fmt.Fprintln(w, expr.Eval(env))
+	})
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
