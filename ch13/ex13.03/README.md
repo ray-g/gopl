c78696f186f17699fcac0dfe732b7c3b9005ee67
Ray Guo <ray_g@live.com>
2017-04-28 01:48:37 +0800

    ex13.3

---
+++ ch13/ex13.03/README.md
@@ -0,0 +1,3 @@
+# Exercise 13.3 (P366)
+
+Use `sync.Mutex` to make `bzip2.writer` safe for concurrent use by multiple goroutines.
---
+++ ch13/ex13.03/bzip2.c
@@ -0,0 +1,16 @@
+/* This file is gopl.io/ch13/bzip/bzip2.c,         */
+/* a simple wrapper for libbzip2 suitable for cgo. */
+#include <bzlib.h>
+
+int bz2compress(bz_stream *s, int action,
+                char *in, unsigned *inlen, char *out, unsigned *outlen) {
+  s->next_in = in;
+  s->avail_in = *inlen;
+  s->next_out = out;
+  s->avail_out = *outlen;
+  int r = BZ2_bzCompress(s, action);
+  *inlen -= s->avail_in;
+  *outlen -= s->avail_out;
+  s->next_in = s->next_out = NULL;
+  return r;
+}
